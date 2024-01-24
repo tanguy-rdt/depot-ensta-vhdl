368e90ee04b8685eccb3505c653c2fabc2aebd26
Tanguy ROUDAUT <tanguy@roudaut.xyz>
2024-01-24 08:43:25 +0100

    update tree

--- uart/correction/uart_write.rb
+++
@@ -1,9 +0,0 @@
-require "uart"
-
-UART.open('/dev/ttyUSB1',19200, '8N1') do |serial|
-  while true
-    print "enter a value > "
-    val=$stdin.gets.chomp.to_i
-    serial.write [val].pack 'C'
-  end
-end
--- uart_vga/correction/compile.x
+++
@@ -1,5 +0,0 @@
-ghdl -a ticks_gen.vhd
-ghdl -a vga_constants.vhd
-ghdl -a vga_controler.vhd
-ghdl -a cross_center_controler.vhd
-ghdl -a soc.vhd
